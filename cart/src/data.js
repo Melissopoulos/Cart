51b1f4b2225309154a3794f691e7b84a61a9054b
George Melissopoulos <melissopoulos.geo@gmail.com>
2023-07-06 14:44:55 +0300

    initial structure of project

---
+++ cart/src/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: "Samsung Galaxy S7",
+    price: 599.99,
+    img: "./images/galaxy.jpg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "google pixel ",
+    price: 499.99,
+    img: "./images/google-pixel.jpg",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Xiaomi Redmi Note 2",
+    price: 699.99,
+    img: "./images/xiaomi.jpg",
+    amount: 1,
+  },
+];
